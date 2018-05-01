093751e0d3a2207b61588809ccc89234ae0d76a9
wadexhong <wadexhong@gmail.com>
2018-05-01 16:46:30 +0800

    set button onClick Log

--- app/src/main/java/com/example/wade8/boxscore/activities/BoxScoreActivity.java
+++ app/src/main/java/com/example/wade8/boxscore/activities/BoxScoreActivity.java
@@ -1,6 +1,7 @@
 package com.example.wade8.boxscore.activities;
 
 import android.app.Activity;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,17 +13,53 @@ import android.widget.ImageView;
 import com.example.wade8.boxscore.BoxScoreContract;
 import com.example.wade8.boxscore.R;
 
-public class BoxScoreActivity extends AppCompatActivity implements BoxScoreContract.View{
+public class BoxScoreActivity extends AppCompatActivity implements BoxScoreContract.View {
     
     private static final String  TAG = BoxScoreActivity.class.getSimpleName();
 
 
-    private ImageView mBackgroundImageView;
+    private ConstraintLayout mMainLayout;
+    private ConstraintLayout mStartGameLayout;
+    private ConstraintLayout mTeamManageLayout;
+    private ConstraintLayout mGameHistoryLayout;
+    private ConstraintLayout mSettingLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_box_score);
+
+        mMainLayout = findViewById(R.id.activity_boxscore_main_layout);
+        mStartGameLayout = findViewById(R.id.activity_boxscore_startgame_layout);
+        mTeamManageLayout = findViewById(R.id.activity_boxscore_teammanage_layout);
+        mGameHistoryLayout = findViewById(R.id.activity_boxscore_gamehistory_layout);
+        mSettingLayout = findViewById(R.id.activity_boxscore_setting_layout);
+        mStartGameLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG,"StartGame pressed");
+            }
+        });
+        mTeamManageLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG,"TeamManage pressed");
+            }
+        });
+        mGameHistoryLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG,"GmaeHistory pressed");
+            }
+        });
+        mSettingLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG,"Setting pressed");
+            }
+        });
+
+
         init();
     }
 
@@ -50,4 +87,5 @@ public class BoxScoreActivity extends AppCompatActivity implements BoxScoreContr
     public void showMainUi() {
 
     }
+
 }
